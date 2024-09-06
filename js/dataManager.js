91d3f985b24c30ce8561b20d8b5dbb2428526d82
mateoperezmesa <132848028+mateoperezmesa@users.noreply.github.com>
2024-09-06 16:05:05 -0500

    Update dataManager.js 9/6/2024 1604
    9/6/2024 1604

--- js/dataManager.js
+++ js/dataManager.js
@@ -1,20 +1,34 @@
 const monthNames = {
-  '12': 'Diciembre',
-  '11': 'Noviembre',
   '01': 'Enero',
   '02': 'Febrero',
-  '03': 'Marzo'
-  // Agrega los nombres de los meses para los demás números de mes aquí
+  '03': 'Marzo',
+  '04': 'Abril',
+  '05': 'Mayo',
+  '06': 'Junio',
+  '07': 'Julio',
+  '08': 'Agosto',
+  '09': 'Septiembre',
+  '10': 'Octubre',
+  '11': 'Noviembre',
+  '12': 'Diciembre'
 };
 
+
 const colorMapping = {
-  '11': 'red',
-  '12': 'orange',
-  '01': '#FFD700',
-  '02': '#008000',
-  '03': '#00A1E1',
-  // ... Asigna colores para los demás meses aquí ...
+  '01': '#FFD700',  // Amarillo dorado para Enero
+  '02': '#008000',  // Verde para Febrero
+  '03': '#00A1E1',  // Azul claro para Marzo
+  '04': '#FF69B4',  // Rosa para Abril
+  '05': '#4B0082',  // Índigo para Mayo
+  '06': '#FF4500',  // Naranja oscuro para Junio
+  '07': '#800080',  // Púrpura para Julio
+  '08': '#4682B4',  // Azul acero para Agosto
+  '09': '#A52A2A',  // Marrón para Septiembre
+  '10': '#FFA500',  // Naranja para Octubre
+  '11': 'red',      // Rojo para Noviembre
+  '12': 'orange'    // Naranja claro para Diciembre
 };
+
 
 const symbolMapping = {
   'Pozo 227-IV-D-103 (250m)': 'circle',
@@ -25,9 +39,16 @@ const symbolMapping = {
   // Agrega más mapeos según necesites para los demás Nombre2
 };
 
-d3.csv("https://raw.githubusercontent.com/hidabril/ISOTOPOS_BOGOTA/main/Resultados_mar2.csv").then(function(data) {
+//Datos Campus (outdated)
+//d3.csv("https://raw.githubusercontent.com/mateoperezmesa/IsotoposBogota/main/data/datos.csv").then(function(data) {
+//Datos ideam
+d3.csv("https://raw.githubusercontent.com/mateoperezmesa/IsotoposBogota/main/data/Isotopos_ideam.csv").then(function(data) {  
+//Datos IAEA
+//d3.csv("https://raw.githubusercontent.com/mateoperezmesa/IsotoposBogota/main/data/isotopos%20iaea.csv").then(function(data) {  
 createButtonsForNombres2(data)
+createYearButtons(data);
 filterDataAndRenderChart()
+
 
 // Crear botones para los "Nombre2"
 function createButtonsForNombres2(data) {
@@ -38,13 +59,13 @@ function createButtonsForNombres2(data) {
   contenedorBotones.innerHTML = '';
 
   nombres2Set.forEach(nombre2 => {
-    const checkbox = document.createElement('input');
+    const checkbox = document.createElement('input'); 
     checkbox.type = 'checkbox';
-    checkbox.id = nombre2;
+    checkbox.id = 'station_' +nombre2;
     checkbox.checked = true; // Por defecto, todos los checkboxes estarán marcados
 
     const label = document.createElement('label');
-    label.htmlFor = nombre2;
+    label.htmlFor = checkbox.id;
     label.appendChild(document.createTextNode(nombre2));
 
     checkbox.addEventListener('change', () => {
@@ -61,17 +82,79 @@ function createButtonsForNombres2(data) {
 }
 
 
+// Crear botones para los "Años"
+function createYearButtons(data) {
+  const yearsSet = new Set(data.map(item => item.Fecha.split('/')[2])); // Suponiendo que la fecha está en formato DD/MM/YYYY
+  const contenedorBotonesAnos = document.getElementById('contenedorBotonesAnos');
+
+  // Limpiar contenido previo
+  contenedorBotonesAnos.innerHTML = '';
+
+  yearsSet.forEach(year => {
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = 'year_' +year;
+    checkbox.checked = true; // Por defecto, todos los checkboxes estarán marcados
+
+    const label = document.createElement('label');
+    label.htmlFor = checkbox.id;
+    label.appendChild(document.createTextNode(year));
+
+    checkbox.addEventListener('change', () => {
+      filterDataAndRenderChart();
+    });
+
+    contenedorBotonesAnos.appendChild(checkbox);
+    contenedorBotonesAnos.appendChild(label);
+    contenedorBotonesAnos.appendChild(document.createElement('br'));
+  });
+
+  // Almacena todos los checkboxes en la variable global para acceder a ellos posteriormente
+  checkboxesAnos = document.querySelectorAll('input[type=checkbox]');
+}
+
+
+
+// Filtrar datos y renderizar grafico
 function filterDataAndRenderChart() {
   const selectedNombre2 = new Set();
   checkboxes.forEach(checkbox => {
-    if (checkbox.checked) {
-      selectedNombre2.add(checkbox.id);
+    if (checkbox.checked && checkbox.id.startsWith('station_')) {
+      selectedNombre2.add(checkbox.id.replace('station_', ''));
     }
   });
 
-  const filteredData1 = data.filter(item => selectedNombre2.has(item.Nombre2));
-  console.log('Datos filtrados:', filteredData1);
+  const selectedYears = new Set();
+  checkboxesAnos.forEach(checkbox => {
+    if (checkbox.checked && checkbox.id.startsWith('year_')) {
+      selectedYears.add(checkbox.id.replace('year_', ''));
+    }
+  });
 
+ 
+
+  const filteredData1 = data
+    .filter(item => selectedNombre2.has(item.Nombre2) && selectedYears.has(item.Fecha.split('/')[2]));
+   
+    // Verirficacion
+   console.log('Estaciones seleccionadas:', Array.from(selectedNombre2));
+   console.log('Años seleccionados:', Array.from(selectedYears));
+   console.log('Datos filtrados:', filteredData1); // Verifica los datos filtrados aquí
+
+
+  // Simbolos
+  const symbols = ['circle', 'triangle-up', 'cross', 'diamond', 'square'];
+  let symbolIndex = 0;
+
+  function getSymbolForStation(nombre2) {
+    if (!symbolMapping[nombre2]) {
+      symbolMapping[nombre2] = symbols[symbolIndex % symbols.length];
+      symbolIndex++;
+    }
+    return symbolMapping[nombre2];
+  }
+ 
+  
 
   // Filtrar los datos específicos de "TOT Hidraulica"
   const d18O_TOT = filteredData1
@@ -86,9 +169,10 @@ function filterDataAndRenderChart() {
   const a = rmaResult.a;
   const b = rmaResult.b;
   const r = rmaResult.rSquared;
-  console.log('a interseccion eje y', a);
-  console.log('b', b);
-  console.log('R^2', r);
+  //console.log('a interseccion eje y', a);
+  //console.log('b', b);
+  //console.log('R^2', r);
+  
 
   // Datos para la ecuacion de la linea meteorica mundiall
   const start = -16; 
@@ -101,16 +185,17 @@ function filterDataAndRenderChart() {
   const uniqueMonths = [...new Set(filteredData1.map(item => item.Fecha.split('/')[1]))];
   const uniqueNombre2 = [...new Set(filteredData1.map(item => item.Nombre2))];
 
+  //console.log('data', filteredData1);
+  console.log(data[0]);
+  console.log('x', filteredData1.map(item => parseFloat(item.d18O)));
+  console.log('y', filteredData1.map(item => parseFloat(item.d2H)));
+  //console.log('s', filteredData1.map(item => symbolMapping[item.Nombre2]));
+  //console.log('c', colorMapping[month]);
+  
   const plotlyData = uniqueMonths.map(month => {
     const filteredData = filteredData1.filter(item => item.Fecha.split('/')[1] === month);
     
-    //console.log('data', filteredData1);
-    //console.log('x', filteredData1.map(item => parseFloat(item.d18O)));
-    //console.log('y', filteredData1.map(item => parseFloat(item.d2H)));
-    //console.log('s', filteredData1.map(item => symbolMapping[item.Nombre2]));
-    //console.log('c', colorMapping[month]);
 
-   
 
     return {  
       type: 'scatter',
@@ -123,7 +208,7 @@ function filterDataAndRenderChart() {
       <br>δ²H ‰: ${parseFloat(item.d2H).toFixed(2)}
       <br>Mes: ${monthNames[item.Fecha.split('/')[1]]}`),
       marker: {
-        symbol: filteredData.map(item => symbolMapping[item.Nombre2]),
+        symbol: filteredData.map(item => getSymbolForStation(item.Nombre2)),
         size: 8,
         color: colorMapping[month],
         line: { // Configura el contorno del marcador
@@ -217,6 +302,7 @@ function filterDataAndRenderChart() {
     layer: 'below',
   });
   
+
   // Línea meteorica mundial
   plotlyData.push({
     type: 'scatter',
